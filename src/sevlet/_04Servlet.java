342e1f8d2900f9dcea3d0f0a0c8b0c51ac3f8554
kickscar <kickscar@gmail.com>
2016-11-09 14:53:51 +0900

    1st commit

---
+++ src/sevlet/_04Servlet.java
@@ -0,0 +1,43 @@
+package sevlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.MemberVo;
+
+@WebServlet("/04")
+public class _04Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost( request, response );
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberVo memberVo2 = new MemberVo();
+		memberVo2.setNo( 2L );
+		memberVo2.setEmail( "kickcar2@gmail.com " );
+		memberVo2.setName( "안대혁" );
+		request.setAttribute( "memberVo2" , memberVo2 );
+
+		MemberVo memberVo3 = new MemberVo();
+		memberVo3.setNo( 3L );
+		memberVo3.setEmail( "kickcar3@gmail.com " );
+		memberVo3.setName( "도우넛" );
+		request.getSession( true ).setAttribute( "memberVo3", memberVo3 );
+
+		MemberVo memberVo4 = new MemberVo();
+		memberVo4.setNo( 4L );
+		memberVo4.setEmail( "kickcar4@gmail.com " );
+		memberVo4.setName( "마이콜" );
+		request.getServletContext().setAttribute( "memberVo4", memberVo4 );
+		
+		request.getRequestDispatcher( "/view/04.jsp" ).forward( request, response );
+	}
+}
